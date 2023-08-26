934feabf30548636f819c100e8a23cb2c7cd8d5a
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-08-26 16:02:22 -0400

    Add files via upload

--- 0125-valid-palindrome.py
+++ 0125-valid-palindrome.py
@@ -1,17 +1,12 @@
-Example 1: aba--- is a palindrome   $abd23abc 
-Input: s = "A man, a plan, a canal: Panama"
-Output: true
-Explanation: "amanaplanacanalpanama" is a palindrome.
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1  ## two pointer 
+        l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.alphanum(s[l]):  ## check if alphanum, na hole increment the left pointer, 1
+            while l < r and not self.alphanum(s[l]):
                 l += 1
             while l < r and not self.alphanum(s[r]):
                 r -= 1
-            if s[l].lower() != s[r].lower():  # only compare when alphanum, otherwise, next pointer 
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
@@ -24,19 +19,3 @@ class Solution:
             or ord("a") <= ord(c) <= ord("z")
             or ord("0") <= ord(c) <= ord("9")
         )
-
-#solution 2
-    s2 = ""
-    for i in s.lower():
-        if(i.isalnum()):
-            s2 += i
-    if(s2 == s2[::-1]):  # The slice starts from the end of the string and moves towards the beginning with a step size of -1/// 
-        return True
-    return False
-
-#####
-original_string = "example"
-reversed_string = original_string[::-1]
-print(reversed_string)
-print result---> elpmaxe
-
