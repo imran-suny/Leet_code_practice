c7b48bc3b7f1158c7f48c4248bd2abcd39cee973
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-08-29 17:23:46 -0400

    Update group anagrams.py

--- group anagrams.py
+++ group anagrams.py
@@ -1,5 +1,3 @@
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 @List : [] ordered and changeable. Allows duplicate members.//
 @Tuple : () used to store multiple items in a single variable// ordered and unchangeable. Allows duplicate members.//
 @Set : set() contructor or {}  to create....unordered, unchangeable*, and unindexed. No duplicate members.//
@@ -9,21 +7,29 @@ example:
 thistuple = { "apple", "banana", "cherry", "apple", "cherry"}-- dictionary, so no duplicate {'apple', 'banana', 'cherry'} but tuple/ () hole all print.
 print(thistuple)
 
+from collections import defaultdict  
+d = defaultdict(lambda: "Not Present")
+d["a"] = 1
+d["b"] = 2 
+print(d["a"])-- 1
+print(d["b"])-- 2
+print(d["c"])-- Not present// [] hobe jodi 'Not Presnet' a 'list' likhi'
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
+        ans = collections.defaultdict(list) # [] print korbe, key na thakle// It provides a default value for the key that does not exists.
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26 #  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
             for c in s:
-                count[ord(c) - ord("a")] += 1
+                count[ord(c) - ord("a")] += 1 # [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
                 # print('count', count)
             ans[tuple(count)].append(s)
             print(ans)
         return ans.values()
-1. 
-
 ### prit
 count [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
 count [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
