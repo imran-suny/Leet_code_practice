8c43a2ed973cd4e73d0b030b35b5ee64e6323e70
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-08-29 16:51:09 -0400

    Add files via upload

---
+++ group anagrams.py
@@ -0,0 +1,16 @@
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+        
