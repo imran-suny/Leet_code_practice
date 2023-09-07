17692f0464f81958af0b060ce92894eab589da5f
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-09-06 20:54:02 -0400

    Update valid parentheses.py

--- valid parentheses.py
+++ valid parentheses.py
@@ -1,1 +1,21 @@
+Problem:
+Input: s = "()[]{}"
+Output: true
+Input: s = "(]"
+Output: false
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
 
