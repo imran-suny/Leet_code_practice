f1dddf52d108b67b02fab5f370625b1866c70073
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-09-06 21:35:41 -0400

    Update valid parentheses.py

--- valid parentheses.py
+++ valid parentheses.py
@@ -6,15 +6,15 @@ Output: false
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        Map = {")": "(", "]": "[", "}": "{"}
-        stack = []
+        Map = {")": "(", "]": "[", "}": "{"} # 
+        stack = [] # 
 
         for c in s:
             if c not in Map:
-                stack.append(c)
-                continue
-            if not stack or stack[-1] != Map[c]:
-                return False
+                stack.append(c) 
+                continue   # append [  ( [ {   ]  # cuase era nai key te  
+            if not stack or stack[-1] != Map[c]:  # if stack not empty and na mille [ stake value != Map value ]  { exist in key value, so pop()-- last er ra '{'
+                 return False
             stack.pop()
 
         return not stack
