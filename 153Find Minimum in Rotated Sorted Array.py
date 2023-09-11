39e661a05789085146c3fd8a85206402d71037c5
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-09-11 14:46:49 -0400

    Update 153Find Minimum in Rotated Sorted Array.py

--- 153Find Minimum in Rotated Sorted Array.py
+++ 153Find Minimum in Rotated Sorted Array.py
@@ -1,6 +1,8 @@
 Input: nums = [3,4,5,1,2]
 Output: 1
 
+# Divide left and right sorted part/// and select a mid pointer also
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
@@ -9,5 +11,5 @@ class Solution:
             if nums[m] > nums[r]:  # 5>2, 
                 l = m + 1     #  l= 2+1=3
             else:
-                r = m   # right pointer chnage to mid 
+                r = m   # right pointer change to mid if nums[m] < nums[r] or nums[m] < nums[l]
         return nums[l]
