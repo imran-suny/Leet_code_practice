3e582397f287bb9c91eb92ba05bbe3e21c9ede28
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2023-09-16 17:56:19 -0400

    Update 121Best Time to Buy and Sell Stock.py

--- 121Best Time to Buy and Sell Stock.py
+++ 121Best Time to Buy and Sell Stock.py
@@ -17,5 +17,25 @@ class Solution:
                 left = right                               # 7,1 = -6// so left ke update left = right and right += 1 ke always update korbo 
             right += 1    # move left pointer to the right position and increment our right pointer by 1. We always want our left point to be minimum.
         return max_profit
+        
+1. Initialize  ‘maxProfit’ to 0 and declare  variable ‘mini’ which we will use to keep track of the buying price (minimum price from day 0 to day i) for selling the stock.
+2. Traverse the array from index 1 to n-1. We started at index 1 because buying and selling the stock on the 0th day will give us a profit of 0
+3. In each iteration, try to find the curProfit. The selling price will be Arr[i] and ‘mini’ will give us the buying price. We calculate the curProfit.
+4. Before going to the next iteration, we check if the current price (Arr[i]) is less than the mini value, and we assign it as the new mini value.
+
+int maximumProfit(vector<int> &Arr){
+	int maxProfit = 0;
+	int mini = Arr[0]; # 
+	for(int i=1;i<Arr.size();i++){
+        int curProfit = Arr[i] - mini; # profit right-left 
+        maxProfit = max(maxProfit,curProfit);
+        mini = min(mini,Arr[i]); # select minimum one 
+        }
+	return maxProfit;
+}
+int main() {
+  vector<int> Arr  ={7,1,5,3,6,4};
+  cout<<"The maximum profit by selling the stock is "<<maximumProfit(Arr);
+}
 
   
