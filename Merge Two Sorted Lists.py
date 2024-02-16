fe0058a5e4526a85bfdb13eb053eecf4ee8e9977
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-16 15:41:47 -0500

    Update Merge Two Sorted Lists.py

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,4 +1,6 @@
 # Iterative
+https://leetcode.com/problems/merge-two-sorted-lists/description/
+
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
         dummy = node = ListNode()
@@ -6,13 +8,13 @@ class Solution:
         while list1 and list2:
             if list1.val < list2.val:
                 node.next = list1
-                list1 = list1.next
+                list1 = list1.next  # update the value nor pointer 
             else:
                 node.next = list2
                 list2 = list2.next
             node = node.next
 
-        node.next = list1 or list2
+        node.next = list1 or list2  # jodi list 1 or 2 sesh na hoy, just add after dummy.next 
 
         return dummy.next
     
