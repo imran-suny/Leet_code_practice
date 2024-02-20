ca6ecf432520f8efb6598f6e8c3b370ffcb6c332
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-20 13:58:37 -0500

    Create Same Tree.py

---
+++ Same Tree.py
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/same-tree/description/
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:  # root not same, False
+            return True
+        if p and q and p.val == q.val:    ## p and q not empty and value is same, check 
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
