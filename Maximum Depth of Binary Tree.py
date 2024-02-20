9c8fb8ab8cad0acb35d186638e10dd4d1002fbb8
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-20 13:14:49 -0500

    Update Maximum Depth of Binary Tree.py

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -1,17 +1,18 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/description/ 
 # RECURSIVE DFS
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))  ## 1 kore add korbe// (3) :1+ max(0, 0)= 1 
 
 # ITERATIVE DFS
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        stack = [[root, 1]]
+        stack = [[root, 1]]  # depth and node store in a stack 
         res = 0
         while stack:
-            node, depth = stack.pop()
+            node, depth = stack.pop()   # pop and add if not empty node /// depth o update 
 
             if node:
                 res = max(res, depth)
@@ -21,12 +22,11 @@ class Solution:
 # BFS
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        q = deque()
+        q = deque()   # add and remove simultanesoulty 
         if root:
             q.append(root)
         level = 0
         while q:
-
             for i in range(len(q)):
                 node = q.popleft()
                 if node.left:
