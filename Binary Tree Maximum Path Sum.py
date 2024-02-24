8416864f27442b4c415e84033f4668b3d73f751f
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-24 15:57:19 -0500

    Update Binary Tree Maximum Path Sum.py

--- Binary Tree Maximum Path Sum.py
+++ Binary Tree Maximum Path Sum.py
@@ -4,18 +4,18 @@ https://leetcode.com/problems/binary-tree-maximum-path-sum/solutions/603423/pyth
     def maxPathSum(self, root: TreeNode) -> int:
 		max_path = float("-inf")                   # placeholder to be updated
  		
-      def get_max_gain(node):
- 			nonlocal max_path                        # This tells that max_path is not a local variable
- 			if node is None:
- 				return 0
+        def get_max_gain(node):
+ 		nonlocal max_path                        # This tells that max_path is not a local variable
+ 		if node is None:
+ 			return 0
 	
- 			gain_on_left = max(get_max_gain(node.left), 0)              # take maximum value between 0 and maximum gain from left branch   -1, -2, -3 > all (-)ve,hence select 0  
- 		  gain_on_right = max(get_max_gain(node.right), 0)            # 
+ 	        gain_on_left = max(get_max_gain(node.left), 0)              # take maximum value between 0 and maximum gain from left branch   -1, -2, -3 > all (-)ve,hence select 0  
+ 		gain_on_right = max(get_max_gain(node.right), 0)            # 
 			
- 		  current_max_path = node.val + gain_on_left + gain_on_right  # for a simple tree, 1,2,3--> 1+2+3= 6    ///split hole 
- 		  max_path = max(max_path, current_max_path)                  # select max, -inf, 6 ...> 6 
+ 		current_max_path = node.val + gain_on_left + gain_on_right  # for a simple tree, 1,2,3--> 1+2+3= 6    ///split hole 
+ 		max_path = max(max_path, current_max_path)                  # select max, -inf, 6 ...> 6 
  			
- 		  return node.val + max(gain_on_left, gain_on_right)          # split na hole, 1,2,3, return 1+3 = 4 
+ 		return node.val + max(gain_on_left, gain_on_right)          # split na hole, 1,2,3, return 1+3 = 4 
 			
  	get_max_gain(root)                                              # Starts the recursion chain
 	return max_path	
