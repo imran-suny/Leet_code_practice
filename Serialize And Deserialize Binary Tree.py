d587ad71a6ecd9b4b1491a49c76ac251c08b6b98
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-25 00:53:19 -0500

    Update Serialize And Deserialize Binary Tree.py

--- Serialize And Deserialize Binary Tree.py
+++ Serialize And Deserialize Binary Tree.py
@@ -1,31 +1,31 @@
 https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+Depth-First Search (DFS)> pre order> root fist, Left> right> kono kichu na thakle 'N'///sob string a 
+
 class Codec:
     def serialize(self, root):
         res = []
-
         def dfs(node):
             if not node:
                 res.append("N")
                 return
-            res.append(str(node.val))
+            res.append(str(node.val))  ##  N an hole add that node /// node.val> node.left> node.right all recursively
             dfs(node.left)
             dfs(node.right)
-
-        dfs(root)
+        dfs(root)                       # recusively call korbo
         return ",".join(res)
 
     def deserialize(self, data):
         vals = data.split(",")
-        self.i = 0
+        self.i = 0                   # split kore index set = 0
 
         def dfs():
-            if vals[self.i] == "N":
+            if vals[self.i] == "N":  # N hole Null node
                 self.i += 1
                 return None
-            node = TreeNode(int(vals[self.i]))
+            node = TreeNode(int(vals[self.i]))  # null na hole node create
             self.i += 1
-            node.left = dfs()
-            node.right = dfs()
-            return node
+            node.left = dfs()                   # left a jabe, f
+            node.right = dfs()                  # right a jabe res theke 
+            return node                         # head 
 
         return dfs()
