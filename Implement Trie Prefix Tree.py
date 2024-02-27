0f3875c9abcb31bc9c92f3f91eccb5836e0a9c11
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-02-27 16:04:14 -0500

    Update Implement Trie Prefix Tree.py

--- Implement Trie Prefix Tree.py
+++ Implement Trie Prefix Tree.py
@@ -2,31 +2,32 @@ https://leetcode.com/problems/implement-trie-prefix-tree/description/
 Input: ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
         [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
 Output:  [null, null, true, false, true, null, true]
+
 class TrieNode:
     def __init__(self):
-        self.children = [None] * 26
-        self.end = False
+        self.children = [None] * 26            # 26 none/ children 
+        self.end = False                       ## mark every node if it is end of a word
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode()                 # create a root node/ trienote 
 
-    def insert(self, word: str) -> None:
-        curr = self.root
-        for c in word:
-            i = ord(c) - ord("a")
-            if curr.children[i] == None:
-                curr.children[i] = TrieNode()
-            curr = curr.children[i]
-        curr.end = True
+    def insert(self, word: str) -> None:       ## if exist or not, na thakle create one 
+        curr = self.root                       # initially start at the root --- set curr
+        for c in word:                         # for evry single character 
+            i = ord(c) - ord("a")              #
+            if curr.children[i] == None:       # if not in hashmap 
+                curr.children[i] = TrieNode()  # create a new empty trinote 
+            curr = curr.children[i]            # if exist set curr to that child
+        curr.end = True                        # after all char from a word, set the cur to True flag///   curr.rnd =True 
 
     def search(self, word: str) -> bool:
         curr = self.root
         for c in word:
             i = ord(c) - ord("a")
-            if curr.children[i] == None:
+            if curr.children[i] == None:  # if a char not exist, return false 
                 return False
-            curr = curr.children[i]
-        return curr.end
+            curr = curr.children[i]       #else finish the word
+        return curr.end   # return true, if curr.end=True
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.root
@@ -34,5 +35,5 @@ class Trie:
             i = ord(c) - ord("a")
             if curr.children[i] == None:
                 return False
-            curr = curr.children[i]
+            curr = curr.children[i]      ## finish the prefix and return true 
         return True
