68d8022f7afc516404bd9a724a3e23c5880a1a44
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-03-04 12:01:58 -0500

    Update Combination Sum.py

--- Combination Sum.py
+++ Combination Sum.py
@@ -13,16 +13,16 @@ class Solution:
                 result.append(curr.copy())
                 return
             
-            if i >= len(candidates) or total > target:   # if index is out of bounds or total sum exceeds the target, stop exploring this path
+            if i >= len(candidates) or total > target:  # if index is out of bounds or total sum exceeds the target, stop exploring this path
                 return
             
             curr.append(candidates[i])               #  Include the current candidate in the combination
             # Explore further combinations with the current candidate
-            dfs(i, curr, total + candidates[i])
+            dfs(i, curr, total + candidates[i]) # dfs(0, [2], 2), dfs(0, [2, 2], 4), dfs(0, [2, 2, 2], 6), dfs(0, [2, 2, 2, 2], 8)... backtrack
             # Backtrack: remove the last candidate to explore combinations without it
-            curr.pop()
+            curr.pop()  # dfs(0, [2, 2, 2], 6) theke 2 remove korar pore abar dfs as it is inside-->  
             # Explore combinations without the current candidate
-            dfs(i + 1, curr, total)
+            dfs(i + 1, curr, total) # dfs(1, [2, 2, 2], 6), dfs(1, [2, 2, 2, 3], 9),  dfs(2, [2, 2, 2, 6], 10),  dfs(3, [2, 2, 2, 7], 13)
         
         dfs(0, [], 0)     # Start DFS traversal from index 0 with an empty current combination and total sum 0
         return result
