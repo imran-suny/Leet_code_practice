dff733e29d57feb51be203b825da17b6e797beb5
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-03-19 16:46:38 -0400

    Update Alien Dictionary.py

--- Alien Dictionary.py
+++ Alien Dictionary.py
@@ -10,38 +10,38 @@ https://www.youtube.com/watch?v=IIgisEKjCKA
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {char: set() for word in words for char in word}
-
+                                                              # make a graph   
+      adj = {char: set() for word in words for char in word}  # w: [],  r : [] 
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            minLen = min(len(w1), len(w2))
+            w1, w2 = words[i], words[i + 1]                   # wrt, wrf 
+            minLen = min(len(w1), len(w2))                    # 3 
             if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
                 return ""
-            for j in range(minLen):
-                if w1[j] != w2[j]:
-                    print(w1[j], w2[j])
-                    adj[w1[j]].add(w2[j])
+            for j in range(minLen):   0,1,2
+                if w1[j] != w2[j]:    w=w, r=r,   t!=f
+                    print(w1[j], w2[j])   
+                    adj[w1[j]].add(w2[j])    #  adj    w: [e], r: [t], t: [f], f:[w], e:[r] 
                     break
 
         visited = {}  # {char: bool} False visited, True current path
         res = []
 
-        def dfs(char):
+        def dfs(char): 
             if char in visited:
-                return visited[char]
+                return visited[char]  
 
             visited[char] = True
 
             for neighChar in adj[char]:
                 if dfs(neighChar):
-                    return True
+                    return True                     #  w not in visited, w:true,      e: true [w r neighbor]               r: t, t: t, f:true              
 
-            visited[char] = False
-            res.append(char)
+            visited[char] = False                   # cycle hole false 
+            res.append(char)                        # f, t,r,e, w
 
         for char in adj:
             if dfs(char):
-                return ""
+                return ""                         # sesh hole w pabe, cycle hole return empty string 
 
         res.reverse()
         return "".join(res)
