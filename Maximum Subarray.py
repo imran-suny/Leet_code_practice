ebc4a80745f667e8127be6b13c93bf22bbd49ec8
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-03-25 15:50:04 -0400

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -5,12 +5,15 @@ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = nums[0]
-        total = 0
-        for n in nums:
-            total += n
-            res = max(res, total)
-            if total < 0:
-                total = 0
-        return res
+        ans = float('-inf')  
+        cur = float('-inf')  
 
+        # Iterate through each element in the array
+        for n in nums:
+            # Update cur to be the maximum of either cur + n or n
+            cur = max(cur + n, n)
+            # Update ans to be the maximum of ans and cur
+            ans = max(ans, cur)
+            
+        return ans  # Return the maximum sum subarray
+        
