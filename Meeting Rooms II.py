8f7687381fd2e359703c8ed6901b2da8507cef08
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-03-27 14:56:32 -0400

    Update Meeting Rooms II.py

--- Meeting Rooms II.py
+++ Meeting Rooms II.py
@@ -2,7 +2,6 @@ Given an array of meeting time intervals consisting of start and end times[[s1,e
 Input: [[0, 30],[5, 10],[15, 20]]
 Output: 2
 
-
 def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         time = []
         for start, end in intervals:
@@ -17,4 +16,24 @@ def minMeetingRooms(self, intervals: List[List[int]]) -> int:
             count += t[1]                           #1, 2, 0,1,0,-1
             max_count = max(max_count, count)       # 1,2,2,2,2,2
         return max_count
+
+
+class Solution:
+  def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    n = len(intervals)
+    ans = 0
+    starts = []
+    ends = []
+    for start, end in intervals:
+      starts.append(start)
+      ends.append(end)
+    starts.sort()  # [0, 5, 15]
+    ends.sort()    # [10, 20, 30]
+    j = 0
+    for i in range(n):
+      if starts[i] < ends[j]:   # sratrt time end time theke kom, mane room lagbe 
+        ans += 1
+      else:
+        j += 1   # update ends if no overlap 
+    return ans
 
