c311133f9f8d3cbd079e2fcec70f204cf10c3def
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-03-29 11:12:42 -0400

    Create Set Matrix Zeroes.py

---
+++ Set Matrix Zeroes.py
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/set-matrix-zeroes/description/
+https://takeuforward.org/data-structure/set-matrix-zero/
+https://leetcode.com/problems/set-matrix-zeroes/solutions/2525398/all-approaches-from-brute-force-to-optimal-with-easy-explanation/
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        # O(1)
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        # determine which rows/cols need to be zero
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0 # first row kore kore felbo 
+                    if r > 0:
+                        matrix[r][0] = 0  # first colum zero
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:   # first row or colum zero hole, sob 0
+                    matrix[r][c] = 0    
+
+        if matrix[0][0] == 0:       
+            for r in range(ROWS):       # first column zero
+                matrix[r][0] = 0
+
+        if rowZero:                      # fisr row zero
+            for c in range(COLS):
+                matrix[0][c] = 0
