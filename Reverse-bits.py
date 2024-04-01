97fca448f9d45e27649d703183200c08481dbe03
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-04-01 13:12:57 -0400

    Update Reverse-bits.py

--- Reverse-bits.py
+++ Reverse-bits.py
@@ -6,8 +6,8 @@ Output:  = 00111001011110000010100101000000
 class Solution:
     def reverseBits(self, n: int):
         res = 0
-        for i in range(32):        
-            bit = (n >> i) & 1        # right shift 32 ghor, then and 1 makes the 0/1.....00000000000000000000000000000
-            res += (bit << (31 - i))  # left shift each bit to its postion 
+        for i in range(32):        01<<1=== shift all bits by 1,,,, 10
+            bit = (n >> i) & 1        # right shift 32 ghor, then and 1 .... count whether 0/1.....00000000000000000000000000100
+            res += (bit << (31 - i))  # left shift each bit to its postion                00100000000000000000000000000
         return res
 
