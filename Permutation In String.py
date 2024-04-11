7ea66c6d10d05eefcff06569434d28b3f0162f78
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-04-11 19:00:04 -0400

    Update Permutation In String.py

--- Permutation In String.py
+++ Permutation In String.py
@@ -8,8 +8,8 @@ class Solution:
 
         s1Count, s2Count = [0] * 26, [0] * 26 
         for i in range(len(s1)):                   # in case same length and equal
-            s1Count[ord(s1[i]) - ord("a")] += 1    # [ 1 1 0 0 0 0 0 0..] 
-            s2Count[ord(s2[i]) - ord("a")] += 1    # [ 1 1 0 0 0 0 0 0..] 
+            s1Count[ord(s1[i]) - ord("a")] += 1    # [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            s2Count[ord(s2[i]) - ord("a")] += 1    # [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         matches = 0
         for i in range(26):
@@ -20,14 +20,14 @@ class Solution:
             if matches == 26:
                 return True
 
-            index = ord(s2[r]) - ord("a")
-            s2Count[index] += 1
+            index = ord(s2[r]) - ord("a")      # right pointer increase 
+            s2Count[index] += 1                 
             if s1Count[index] == s2Count[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s1Count[index] + 1 == s2Count[index]:   # 1 ta beshi holei komabo  2
                 matches -= 1
 
-            index = ord(s2[l]) - ord("a")
+            index = ord(s2[l]) - ord("a")        # left drcrease pointer increase
             s2Count[index] -= 1
             if s1Count[index] == s2Count[index]:
                 matches += 1
