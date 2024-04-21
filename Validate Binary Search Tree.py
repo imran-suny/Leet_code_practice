916875367d9ac734991409bd553ef8035c9e1625
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-04-20 23:21:20 -0400

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -5,9 +5,11 @@ class Solution:
         def valid(node, left, right):
             if not node:
                 return True
-            if not (left < node.val < right):                                                  # -inf < node.val < +inf
+            if not (left < node.val < right):                                  # -inf < node.val < +inf
                 return False                               
 
-            return valid(node.left, left, node.val) and valid( node.right, node.val, right)   # jake check seta first, left boundary(-inf), main mode value/// jake check seta first, main mode value, right boundary(+inf)
+            return valid(node.left, left, node.val) and valid( node.right, node.val, right)   
+            # jake check seta first, left boundary(-inf), main mode value///
+            #jake check seta first, main mode value, right boundary(+inf)
 
         return valid(root, float("-inf"), float("inf"))
