0346b0f7c513cd940922882aa9a8a9b61996d6b5
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-05-08 14:10:47 -0400

    Update Max Area of Island.py

--- Max Area of Island.py
+++ Max Area of Island.py
@@ -3,7 +3,7 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
 
-        def dfs(r, c):
+        def dfs(r, c):              # If the cell is out of bounds or water (0) or already visited, return 0
             if (
                 r < 0
                 or r == ROWS
@@ -14,10 +14,11 @@ class Solution:
             ):
                 return 0
             visit.add((r, c))
-            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)        # Explore adjacent cells in all four directions and count the area
 
         area = 0
         for r in range(ROWS):
             for c in range(COLS):
-                area = max(area, dfs(r, c))
+                if grid[r][c] == 1 and (r, c) not in visit:
+                    area = max(area, dfs(r, c))
         return area
