d6237668bc001bada7665aebee6c92a4fee29750
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-05-14 14:58:34 -0400

    Update Reconstruct Itinerary.py

--- Reconstruct Itinerary.py
+++ Reconstruct Itinerary.py
@@ -4,17 +4,28 @@ class Solution:
         graph = defaultdict(deque)
         for src, dst in sorted(tickets):                    # Sort tickets lexicographically and populate the graph
             graph[src].append(dst)
-        
+     # print(graph): defaultdict(<class 'collections.deque'>, {'JFK': deque(['ACL', 'MUC']), 'LHR': deque(['SFO']), 'MUC': deque(['LHR']), 'SFO': deque(['SJC'])})
         itinerary = []     
         def dfs(airport):
             while graph[airport]:
                 next_airport = graph[airport].popleft()  # Get the next destination
                 dfs(next_airport)
-            itinerary.append(airport)
-        
+            itinerary.append(airport)  #  print('gggggg', itinerary)
+            
         dfs("JFK")
-        return itinerary[::-1]
+        return itinerary[::-1] 
 
 # sol = Solution()
-# tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
-# print(sol.findItinerary(tickets))  # Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+# tickets =  [["MUC", "LHR"], ["JFK", "MUC"], ["JFK", "ACL"], ["SFO", "SJC"], ["LHR", "SFO"]]
+# print(sol.findItinerary(tickets))  # Output: ['JFK', 'MUC', 'LHR', 'SFO', 'SJC', 'ACL']
+deque(['ACL', 'MUC'])
+gggggg ['ACL']
+deque(['MUC'])
+deque(['LHR'])
+deque(['SFO'])
+deque(['SJC'])
+gggggg ['ACL', 'SJC']
+gggggg ['ACL', 'SJC', 'SFO']
+gggggg ['ACL', 'SJC', 'SFO', 'LHR']
+gggggg ['ACL', 'SJC', 'SFO', 'LHR', 'MUC']
+gggggg ['ACL', 'SJC', 'SFO', 'LHR', 'MUC', 'JFK']
