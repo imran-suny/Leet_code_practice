884ba759465b727efc398df0056f59bf2c2261e9
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-05-15 16:36:18 -0400

    Create Swim In Rising Water.py

---
+++ Swim In Rising Water.py
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/swim-in-rising-water/description/
+https://leetcode.com/problems/swim-in-rising-water/solutions/4748318/93-1-approach-1-o-n-2-log-n-python-c-step-by-step-explanation/
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        visit = set()
+        minH = [[grid[0][0], 0, 0]]  # (time/max-height, r, c)
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        visit.add((0, 0))
+        while minH:
+            t, r, c = heapq.heappop(minH)
+            if r == N - 1 and c == N - 1:
+                return t
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if (
+                    neiR < 0
+                    or neiC < 0
+                    or neiR == N
+                    or neiC == N
+                    or (neiR, neiC) in visit
+                ):
+                    continue
+                visit.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])        # Push the neighboring cell into the min-heap with updated time/max-height
