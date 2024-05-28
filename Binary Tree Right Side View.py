ddd3f0eb3fc870de09f1139414979daa81edeb33
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-05-28 17:31:08 -0400

    Update Binary Tree Right Side View.py

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -2,23 +2,27 @@ https://leetcode.com/problems/binary-tree-right-side-view/description/
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
-            return []
-        
-        queue = deque([root])
+            return []        
+        queue = deque([root])                 # q= [1, ]
         res = []
 
         while queue:
-            level_length = len(queue)
+            level_length = len(queue)       # 1   2nd: len = 2
 
             for i in range(level_length):
-                node = queue.popleft()
+                node = queue.popleft()             # 2 pop kolre [3] thakbe 
                 
-                if i == level_length - 1:
-                    res.append(node.val)
+                if i == level_length - 1:   # 0= 1-1    , 0= 2-1
+                    res.append(node.val)    # res = [1], 3 append hobe on 2nd loop 
                 
-                if node.left:
-                    queue.append(node.left)
+                if node.left:               # node.left = 2 
+                    queue.append(node.left) # q = [2,]
                 if node.right:
-                    queue.append(node.right)
-
+                    queue.append(node.right) # q = [2, 3]  q = [4]
         return res
+        
+#       1
+#      / \
+#     2   3
+#      \   \
+#       5   4
