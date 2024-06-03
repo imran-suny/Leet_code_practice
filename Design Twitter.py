9db15b143e176a5d49115f4e084b9b140c3b84f8
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-06-03 13:38:01 -0400

    Update Design Twitter.py

--- Design Twitter.py
+++ Design Twitter.py
@@ -47,3 +47,45 @@ class Twitter:
         # Remove the followeeId from the set of users followed by the followerId
         if followeeId in self.followMap[followerId]:
             self.followMap[followerId].remove(followeeId)
+
+
+User 1 follows User 2 and User 3.
+Tweet data:
+User 1's tweets: [(3, 101), (2, 3), (1, 5)]
+User 2's tweets: [(4, 201), (3, 102)]
+User 3's tweets: [(5, 301), (2, 6)]
+
+minHeap initially contains:
+[(3, 101, 1, 1)] from User 1   (index-1 = 2-1=2 , index -1 =2-1=1)
+[(4, 201, 2, 0)] from User 2
+[(5, 301, 3, 0)] from User 3
+Iteration 1:
+Pop the smallest element: [3, 101, 1, 1] from User 1....Add 101 to res.
+Push the next tweet from User 1: [2, 3, 1, 0].
+Iteration 2:
+Pop the smallest element: [2, 3, 1, 0] from User 1.
+Add 3 to res.
+Push the next tweet from User 1: [1, 5, 1, -1].
+Iteration 3:
+Pop the smallest element: [1, 5, 1, -1] from User 1.
+Add 5 to res.
+No more tweets from User 1 to push.
+Iteration 4:
+Pop the smallest element: [4, 201, 2, 0] from User 2.
+Add 201 to res.
+Push the next tweet from User 2: [3, 102, 2, -1].
+Iteration 5:
+Pop the smallest element: [3, 102, 2, -1] from User 2.
+Add 102 to res.
+No more tweets from User 2 to push.
+Iteration 6:
+Pop the smallest element: [5, 301, 3, 0] from User 3.
+Add 301 to res.
+Push the next tweet from User 3: [2, 6, 3, -1].
+Iteration 7:
+Pop the smallest element: [2, 6, 3, -1] from User 3.
+Add 6 to res.
+No more tweets from User 3 to push.
+Final res List:
+The res list will contain the 10 most recent tweet IDs (or fewer if there aren't 10 tweets), ensuring they are displayed in the correct order of recency.
+In this example, the res list would be [301, 201, 102, 101, 3, 6, 5]
