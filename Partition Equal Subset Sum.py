a54ac372155394fbc1f5127ffc6b8cd29e981cdc
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-06-05 14:43:56 -0400

    Update Partition Equal Subset Sum.py

--- Partition Equal Subset Sum.py
+++ Partition Equal Subset Sum.py
@@ -1,3 +1,4 @@
+https://leetcode.com/problems/partition-equal-subset-sum/solutions/4775884/105-1-approach-1-o-n-target-sum-python-c-step-by-step-explanation/
 Input: nums = [1,5,11,5]
 Output: true
 Explanation: The array can be partitioned as [1, 5, 5] and [11].
