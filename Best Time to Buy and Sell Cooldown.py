e82c90c68ace744a36312dd9fb1f6a134e8e882b
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-06-23 13:58:00 -0400

    Create  Best Time to Buy and Sell Cooldown.py

---
+++ Best Time to Buy and Sell Cooldown.py
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memory = {}  # Dictionary to store memoization results
+
+        def dfs(i, buying):
+            if i >= len(prices): # Base case: if we have reached the end of prices list
+                return 0
+            if (i, buying) in memory:                    # If the result for the current state is already memoized, return it
+                return memory[(i, buying)]
+
+            # Explore the cooldown state
+            cooldown = dfs(i + 1, buying)
+            
+            if buying:
+                                        # If in buying state, we have two options:
+                                        # 1. Buy the stock and move to the next day in selling state
+                                        # 2. Skip buying and remain in buying state (cooldown)
+                buy = dfs(i + 1, not buying) - prices[i]
+                                        # Select the option that maximizes profit
+                memory[(i, buying)] = max(buy, cooldown)
+            else:
+                # If in selling state, we have two options:
+                # 1. Sell the stock and skip the cooldown day
+                # 2. Skip selling and move to the next day in cooldown state
+                sell = dfs(i + 2, not buying) + prices[i]
+                # Select the option that maximizes profit
+                memory[(i, buying)] = max(sell, cooldown)
+            # Return the maximum profit achievable for the current state
+            return memory[(i, buying)]
+
+        # Start the DFS from day 0 with buying state
+        return dfs(0, True)
