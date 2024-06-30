a3d3307f093aaf37230d148da9065e6b95ca16d2
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-06-30 15:05:42 -0400

    Update Coin Change II.py

--- Coin Change II.py
+++ Coin Change II.py
@@ -20,10 +20,17 @@ class Solution:
 
         # DYNAMIC PROGRAMMING # Time: O(n*m)  # Memory: O(n*m)
         dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-        dp[0] = [1] * (len(coins) + 1)  # [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        dp[0] = [1] * (len(coins) + 1)            # [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         for a in range(1, amount + 1):
             for i in range(len(coins) - 1, -1, -1):
                 dp[a][i] = dp[a][i + 1]
-                if a - coins[i] >= 0:
+                if a - coins[i] >= 0:             #  if current coin denomination can contribute to the amount a
                     dp[a][i] += dp[a - coins[i]][i]
         return dp[amount][0]
+Final DP Table:
+[1, 1, 1, 1]
+[1, 0, 0, 0]  # dp[1][2]= dp[1][3] =0
+[2, 1, 0, 0]
+[2, 0, 0, 0]
+[3, 1, 0, 0]
+[4, 1, 1, 0]
