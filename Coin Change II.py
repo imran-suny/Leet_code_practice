3102869ad004687adcbd693d760267f907eb44c0
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-06-30 15:11:31 -0400

    Update Coin Change II.py

--- Coin Change II.py
+++ Coin Change II.py
@@ -20,16 +20,16 @@ class Solution:
 
         # DYNAMIC PROGRAMMING # Time: O(n*m)  # Memory: O(n*m)
         dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-        dp[0] = [1] * (len(coins) + 1)            # [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        dp[0] = [1] * (len(coins) + 1)               # [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         for a in range(1, amount + 1):
-            for i in range(len(coins) - 1, -1, -1):
+            for i in range(len(coins) - 1, -1, -1):  #2,1,0
                 dp[a][i] = dp[a][i + 1]
-                if a - coins[i] >= 0:             #  if current coin denomination can contribute to the amount a
+                if a - coins[i] >= 0:                #  if current coin denomination can contribute to the amount a
                     dp[a][i] += dp[a - coins[i]][i]
         return dp[amount][0]
 Final DP Table:
 [1, 1, 1, 1]
-[1, 0, 0, 0]  # dp[1][2]= dp[1][3] =0
+[1, 0, 0, 0]  # dp[1][2]= dp[1][3] =0    dp[a][i] = dp[a][i + 1]  ### 1 er jonno,,,  dp[a][i] += dp[a - coins[i]][i]    dp[1-1][i],, dp[0][0]
 [2, 1, 0, 0]
 [2, 0, 0, 0]
 [3, 1, 0, 0]
