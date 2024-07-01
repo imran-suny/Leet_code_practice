99ef519d01b9885854d985d4c2b5fbb90f0aec87
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-01 12:28:42 -0400

    Update Distinct Subsequences.py

--- Distinct Subsequences.py
+++ Distinct Subsequences.py
@@ -3,6 +3,29 @@ Output: 3
 
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+        # Memoization dictionary
+        memo = {}
+        
+        def dfs(i, j):
+            if j == len(t):  # If we've matched all characters of t
+                return 1
+            if i == len(s):  # If we've exhausted all characters of s
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+        
+            if s[i] == t[j]:
+                # If the characters match, either use the character or skip it
+                memo[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            else:
+                # If they don't match, skip the current character of s
+                memo[(i, j)] = dfs(i + 1, j)   
+            return memo[(i, j)]
+        
+        return dfs(0, 0)
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
         cache = {}
 
         for i in range(len(s) + 1):
