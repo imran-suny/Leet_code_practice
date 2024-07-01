2e9347a1bf0925367243e9456bf55e17f60b1a2d
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-01 15:52:53 -0400

    Update Burst Balloons.py

--- Burst Balloons.py
+++ Burst Balloons.py
@@ -1,16 +1,11 @@
-Input: nums = [3,1,5,8]
-Output: 167
-Explanation:
+Input: nums = [3,1,5,8]  Output: 167 O(n^3)
 nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
 coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:   
         nums = [1] + nums + [1]              # Add dummy balloons with value 1 at the beginning and end of the `nums` array
         memory = {}
-
-        # Define a recursive function `dfs` to compute maximum coins within a range [l, r]
         def dfs(l, r):
-            # Base case: If l > r, return 0
             if l > r:
                 return 0
             if (l, r) in memory:
@@ -20,12 +15,10 @@ class Solution:
             memory[(l, r)] = 0
             # Iterate over all possible choices of bursting balloons within the range [l, r]
             for i in range(l, r + 1):
-                coins = nums[l - 1] * nums[i] * nums[r + 1]
-                coins += dfs(l, i - 1) + dfs(i + 1, r)
+                coins = nums[l - 1] * nums[i] * nums[r + 1]  # if nums[i] pops last
+                coins += dfs(l, i - 1) + dfs(i + 1, r)        # left and right sub array 
                 # Update maximum coins for the current range [l, r]
                 memory[(l, r)] = max(memory[(l, r)], coins)
             
             return memory[(l, r)]
-        
-        # Call the dfs function with the initial range [1, len(nums) - 2]
-        return dfs(1, len(nums) - 2)
+        return dfs(1, len(nums) - 2)     # nums = [1] + nums + [1]  , len(nums) - 1 gives last value, so -2 gives nums
