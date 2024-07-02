55ce199ff3cd60ea64cbcb0e0e1675c03fe6fd6b
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-02 13:46:40 -0400

    Create Jump Game II.py

---
+++ Jump Game II.py
@@ -0,0 +1,16 @@
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        res = 0
+        while r < (len(nums) - 1):
+            maxJump = 0
+            for i in range(l, r + 1):
+                maxJump = max(maxJump, i + nums[i])
+            l = r + 1
+            r = maxJump
+            res += 1
+        return res
