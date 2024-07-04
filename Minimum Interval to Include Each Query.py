74e5bfe7427a169368420b80c317fc682ffdc641
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-04 13:29:22 -0400

    Create Minimum Interval to Include Each Query.py

---
+++ Minimum Interval to Include Each Query.py
@@ -0,0 +1,23 @@
+Input: intervals = [[1,4],[2,4],[3,6],[4,4]], queries = [2,3,4,5]     Output: [3,3,1,4]
+Explanation: The queries are processed as follows:
+- Query = 2: The interval [2,4] is the smallest interval containing 2. The answer is 4 - 2 + 1 = 3.
+- Query = 3: The interval [2,4] is the smallest interval containing 3. The answer is 4 - 2 + 1 = 3.
+- Query = 4: The interval [4,4] is the smallest interval containing 4. The answer is 4 - 4 + 1 = 1.
+- Query = 5: The interval [3,6] is the smallest interval containing 5. The answer is 6 - 3 + 1 = 4.
+
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort()
+        minHeap = []
+        res = {}
+        i = 0
+        for q in sorted(queries):
+            while i < len(intervals) and intervals[i][0] <= q:   # left value choto hole add
+                l, r = intervals[i]
+                heapq.heappush(minHeap, (r - l + 1, r))
+                i += 1
+
+            while minHeap and minHeap[0][1] < q:       # right choto hole pop from minH
+                heapq.heappop(minHeap)
+            res[q] = minHeap[0][0] if minHeap else -1
+        return [res[q] for q in queries]
