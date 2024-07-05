d79c667c4987648a3ac8b12464b8d3c984c74f98
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-05 18:32:38 -0400

    Create Pow.py

---
+++ Pow.py
@@ -0,0 +1,19 @@
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            res = helper(x * x, n // 2)
+            return x * res if n % 2 else res
+
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
