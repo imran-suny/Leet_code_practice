a52d4531088b67606d21b9be2748f39c53ce9781
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-15 19:13:37 -0400

    Update Sliding Window Maximum.py

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -1,45 +1,29 @@
-Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-Output: [3,3,5,5,6,7]
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3 Output: [3,3,5,5,6,7]
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        ans = []                # List to store the maximum values for each sliding window
-        q = collections.deque()  # Deque to store indices of elements in the current window
-        left = right = 0         # Pointers for the sliding window
-
-        while right < len(nums):
-            # Pop smaller values from the back of the deque
-            while q and nums[q[-1]] < nums[right]:
+        output = []
+        q = collections.deque()  # index
+        l = r = 0
+        # O(n) O(n)
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]: # pop smaller values from q
                 q.pop()
-            q.append(right)
-
-            # Remove the leftmost value if it's outside the current window
-            if left > q[0]:
+            q.append(r)
+           
+            if l > q[0]:   # remove left val from window
                 q.popleft()
 
-            # If the window size is reached, append the maximum value to the result
-            if (right + 1) >= k:
-                ans.append(nums[q[0]])
-                left += 1
-            right += 1
+            if (r + 1) >= k: If the window size is reached, append the maximum value to the result
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+        return output
 
-        return ans
-        
-Initialization:
-output = []
-r = 0:
-nums[0] = 1
-q = deque([0])
+Initial--- output = [] r = 0: nums[0] = 1 -- q.append(right)-- q = deque([0])
+r = 1: r=1 < len(nums)=8 -->  nums[q[-1]] nums[0]1< nums[r]3... O pop, q = deque([1]) (after popping index 0)
 
-r = 1:
-nums[1] = 3
-q = deque([1]) (after popping index 0)
-
-r = 2:
-nums[2] = -1
-q = deque([1, 2])
-Window size is 3, append nums[1] = 3 to output.
-output = [3]
-l = 1
+r = 2: nums[2] = -1 q = deque([1, 2])    Window size is 3, append nums[q[0]  nums[1] = 3 to output.   output = [3]
+l = 1 # only when while works, window update 
 
 r = 3:
 nums[3] = -3
