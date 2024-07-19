51e63e8f650b2b08a52f9eb08afae0b45c724c93
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-19 17:12:20 -0400

    Update Largest Rectangle In Histogram.py

--- Largest Rectangle In Histogram.py
+++ Largest Rectangle In Histogram.py
@@ -8,10 +8,13 @@ class Solution:
             start = i
             while stack and stack[-1][1] > h:
                 index, height = stack.pop()
-                maxArea = max(maxArea, height * (i - index))
-                start = index
+                maxArea = max(maxArea, height * (i - index)) # calculate width of rectangle by subtracting starting index  from current index.
+                start = index  # Push the current bar's index
             stack.append((start, h))
-
+            
+#After processing all bars, there may still be bars left in the stack. For each remaining bar in the stack,
+#calculate the area using the height of the bar and the difference between the current index and the index at the top of the stack.
+        
         for i, h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
