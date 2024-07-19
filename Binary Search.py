83808242da5b39e3de9cf94efdeab85eafa6ae15
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-19 18:13:50 -0400

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,16 @@
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)  # (l + r) // 2 can lead to overflow  #  In Python, integers are arbitrarily large, so this isn't an issue
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+
