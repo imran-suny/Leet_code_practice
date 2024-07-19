f497866fcf2693f36116cfbd9d4ed60b70648e02
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-19 00:38:15 -0400

    Create Min Stack.py

---
+++ Min Stack.py
@@ -0,0 +1,24 @@
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+[null,null,null,null,-3,null,0,-2]
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val) #minStack  reflects the minimum element at each step.
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
