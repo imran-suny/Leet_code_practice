573a699a2c0699aec44c96d3f21afec772426d14
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-22 17:49:23 -0400

    Update Time Based Key Value Store.py

--- Time Based Key Value Store.py
+++ Time Based Key Value Store.py
@@ -5,7 +5,7 @@ Output:
 
 class TimeMap:
     def __init__(self):
-        self.keyStore = {}  # key : list of [val, timestamp]
+        self.keyStore = {}  ## Initialize a defaultdict with lists to store key-value pairs.
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.keyStore:
@@ -13,11 +13,12 @@ class TimeMap:
         self.keyStore[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.keyStore.get(key, [])
+        res = ""
+        values =  self.keyStore.get(key, []) # key thakle value  ["foo", 1], ["foo", 3] ,  na hole empty list 
         l, r = 0, len(values) - 1
         while l <= r:
             m = (l + r) // 2
-            if values[m][1] <= timestamp:
+            if values[m][1] <= timestamp:  # # If the timestamp at mid is less than or equal to the target timestamp
                 res = values[m][0]
                 l = m + 1
             else:
