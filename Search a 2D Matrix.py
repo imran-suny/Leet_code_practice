6a9951634e033d3c604af74a9d5571e26ebef134
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-22 14:23:14 -0400

    Update Search a 2D Matrix.py

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -5,10 +5,10 @@ class Solution:
 
         top, bot = 0, ROWS - 1          ##Binary Search for the Correct Row:
         while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
+            row = (top + bot) // 2            # the target is greater than the largest element in the current row.
+            if target > matrix[row][-1]:     # target> last element, target cannot be in this row or any row before it, so the search continues in rows below
+                top = row + 1           
+            elif target < matrix[row][0]:    #  if the target is less than the smallest element in the current row.
                 bot = row - 1                 # row = (0 + 0) // 2 = 0
             else:
                 break
