e9fb1681221d01661debb444a9ebbc7510356242
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-22 15:03:35 -0400

    Update Koko Eating Bananas.py

--- Koko Eating Bananas.py
+++ Koko Eating Bananas.py
@@ -1,17 +1,19 @@
 https://leetcode.com/problems/koko-eating-bananas/description/
+Input: piles = [3,6,7,11], h = 8
+Output: 4 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+        l, r = 1, max(piles)  # 
+        res = r  # maximum koto hour lagbe ?11 in this case 
 
         while l <= r:
-            k = (l + r) // 2
+            k = (l + r) // 2   # 1+11//2 = 6
 
             totalTime = 0
             for p in piles:
-                totalTime += math.ceil(float(p) / k)
+                totalTime += math.ceil(float(p) / k)  # piles r value vag korbo 
             if totalTime <= h:
-                res = k
+                res = k      # res update, 6 hobe initial 
                 r = k - 1
             else:
                 l = k + 1
