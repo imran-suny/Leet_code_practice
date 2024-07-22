a1661b359e9cf8214bbc7c75dd3e1217ad67aa83
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-21 20:27:27 -0400

    Create Add Two Numbers.py

---
+++ Add Two Numbers.py
@@ -0,0 +1,27 @@
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+https://leetcode.com/problems/add-two-numbers/description/
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
