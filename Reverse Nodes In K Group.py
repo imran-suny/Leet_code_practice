c8e5504396b6b80098bf1ddd49add016e9f51ae3
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-24 09:26:17 -0400

    Update Reverse Nodes In K Group.py

--- Reverse Nodes In K Group.py
+++ Reverse Nodes In K Group.py
@@ -1,26 +1,26 @@
 https://leetcode.com/problems/reverse-nodes-in-k-group/description/
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        dummy = ListNode(0, head)
-        groupPrev = dummy
+        dummy = ListNode(0, head)          #A dummy node is created to simplify edge cases (e.g., reversing the first group).
+        groupPrev = dummy              # groupPrev is initialized to point to the dummy node
 
-        while True:
+        while True:              # The loop continues until there are no more groups of k nodes to reverse.
             kth = self.getKth(groupPrev, k)
-            if not kth:
+            if not kth:     # kth is k-th node from groupPrev. If fewer than k nodes remaining, kth will be None, and the loop breaks.
                 break
-            groupNext = kth.next
+            groupNext = kth.next   # next group is the node following kth
 
             # reverse group
-            prev, curr = kth.next, groupPrev.next
+            prev, curr = kth.next, groupPrev.next   # kth.next re last pointer 1>2>3, curr is first node of current group[dummy.next=1]
             while curr != groupNext:
                 tmp = curr.next
                 curr.next = prev
                 prev = curr
                 curr = tmp
 
-            tmp = groupPrev.next
-            groupPrev.next = kth
-            groupPrev = tmp
+            tmp = groupPrev.next 
+            groupPrev.next = kth  #updated to point to the new head of the reversed group (kth)
+            groupPrev = tmp    #  updated to point to the last node of the reversed group (tmp)
         return dummy.next
 
     def getKth(self, curr, k):
