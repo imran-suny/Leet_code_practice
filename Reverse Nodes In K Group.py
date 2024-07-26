688337eb52331e34aad1cf99afcc144c565771be
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-26 10:15:04 -0400

    Update Reverse Nodes In K Group.py

--- Reverse Nodes In K Group.py
+++ Reverse Nodes In K Group.py
@@ -2,14 +2,14 @@ https://leetcode.com/problems/reverse-nodes-in-k-group/description/
 https://leetcode.com/problems/reverse-nodes-in-k-group/solutions/4672340/44-1-approach-1-o-n-python-c-step-by-step-explanation/
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        dummy = ListNode(0, head)      #  reversing the  group
-        groupPrev = dummy              # groupPrev is initialized to point to the dummy node
+        dummy = ListNode(0, head)     
+        groupPrev = dummy                                  # groupPrev is  dummy node
 
-        while True:              # The loop continues until there are no more groups of k nodes to reverse.
+        while True:                                        # loop continues until no more groups of k
             kth = self.getKth(groupPrev, k)
-            if not kth:     # kth is k-th node from groupPrev. If fewer than k nodes remaining, kth will be None, and the loop breaks.
+            if not kth:                                     # kth node from groupPrev. If fewer than k nodes remaining, the loop breaks.
                 break
-            groupNext = kth.next   # next group is the node following kth
+            groupNext = kth.next                             # next group is the node following kth
 
             # reverse group
             prev, curr = kth.next, groupPrev.next   # kth.next re last pointer 1>2>3, curr is first node of current group[dummy.next=1]
@@ -19,10 +19,10 @@ class Solution:
                 prev = curr
                 curr = tmp
                 
-           # Connect the reversed group to the rest of the list.
-            tmp = groupPrev.next 
-            groupPrev.next = kth           #updated to point to the new head of the reversed group (kth)
-            groupPrev = tmp                #  updated to point to the last node of the reversed group (tmp)
+           # Updating Pointers After Reversing//// Connect the reversed group to the rest of the list.
+            tmp = groupPrev.next                     # first node of the current k-group.
+            groupPrev.next = kth                     # dum 0>1>2, groupprev=dummy, groupprev.next =1, 0>2>1-->point-->kth  
+            groupPrev = tmp                          #  groupPrev is then updated to tmp (1), which is the last node of the reversed group.
         return dummy.next
 
     def getKth(self, curr, k):
