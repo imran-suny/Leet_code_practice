2f8178db1bfb124e4a820021e327baaa4dbb293a
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-07-29 15:11:00 -0400

    Update Product of Array Except Self.py

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -6,17 +6,17 @@ Input: nums = [1,2,3,4]  # num[i]-- * mul(i+1 to len (num)-1, )
 Output: [24,12,8,6] # answer [i= ]
 
 nums = [1,2,3,4]
-res = [1] * (len(nums))           #[1, 1, 1, 1]
+res = [1] * (len(nums))              #[1, 1, 1, 1]
 
-for i in range(1, len(nums)):     # prefix means left to right multiplication, first value 1 as no prefix (2nd to last 1,2,3)
-    res[i] = res[i-1] * nums[i-1] # res [1 x 1 1] = res[0]*num[0] = 1*1 = x te boshbe  
-    print('pre', res)             # res [1 1 x 1] = res[1]*num[1] = 1*2 = x te boshbe
+for i in range(1, len(nums)):        # prefix means left to right multiplication, first value 1 as no prefix (2nd to last 1,2,3)
+    res[i] = res[i-1] * nums[i-1]    # res [1 x 1 1] = res[0]*num[0] = 1*1 = x te boshbe  
+    print('pre', res)                # res [1 1 x 1] = res[1]*num[1] = 1*2 = x te boshbe
 postfix = 1
-for i in range(len(nums) - 1, -1, -1):# postfix reverse mul [24,24,12,4] // for each mul Prefix*Postfix
+for i in range(len(nums) - 1, -1, -1):   # postfix reverse mul [24,24,12,4] // for each mul Prefix*Postfix
     res[i] = res[i] * postfix                 # to keep last position fix, mul with 1. postfix =1 res[4] = res[4]*1= res[4]
-    print('post', res)                # multiplies the current value at index i in the res with the current value of the postfix variable.
+    print('post', res)                   # multiplies the current value at index i in the res with the current value of the postfix variable.
     postfix = postfix * nums[i]                # portfix = 1 * num[last] = 1*4 = 4 
-    print('postfix', postfix)         # 4*2= 8 
+    print('postfix', postfix)            # 4*2= 8 
 print( res)
 
 Output:::: 
