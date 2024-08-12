e099a194ba4aa8d5f2c77f0be8606360bdc0515a
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-08-11 23:38:10 -0400

    Update Subtree of Another Tree.py

--- Subtree of Another Tree.py
+++ Subtree of Another Tree.py
@@ -1,12 +1,12 @@
 class Solution:
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not t:   # t-- subtree 
+        if not t:   # subtree null hole true, cause main tree te null thakbe 
             return True
-        if not s:   # s main tree 
+        if not s:   # main tree empty hole False
             return False
-        if self.sameTree(s, t):
+        if self.sameTree(s, t):   # root same kina ?
             return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)    # s.left / s.right kina ??
 
     def sameTree(self, s, t):
         if not s and not t:
