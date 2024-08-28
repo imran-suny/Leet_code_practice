f85a1c9ca18de41908385384931493517b4ee0cb
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-08-28 13:08:17 -0400

    Update Redundant Connection.py

--- Redundant Connection.py
+++ Redundant Connection.py
@@ -4,12 +4,12 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         par = [i for i in range(len(edges) + 1)]
         rank = [1] * (len(edges) + 1)
-        def find(n):
-            p = par[n]
-            while p != par[p]:
-                par[p] = par[par[p]]
-                p = par[p]
-            return p
+
+        def find(self, n):
+            if par[n] != n:
+                par[n] = self.find(par[n])  # Recursively find the root and apply path compression
+         return par[n]
+
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
 
