a9b51537a31b64af27decfb0913d1148d66a1c40
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-09-08 09:37:36 -0400

    Update Pow.py

--- Pow.py
+++ Pow.py
@@ -13,7 +13,7 @@ class Solution:
                 return 1
 
             res = helper(x * x, n // 2)
-            return x * res if n % 2 else res
+            return x * res if n % 2 else res   # helper(4, 0), n == 0, return 1. Going back helper(2, 1), res = 1. Since n % 2 = 1%2=1, return x * res, which is 2 * 1 = 2.
 
         res = helper(x, abs(n))
         return res if n >= 0 else 1 / res
