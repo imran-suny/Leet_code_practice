a51dedc5bf4ecbd2d6622d229e94b802a1098f2a
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-14 17:34:44 -0400

    Update Product of Array Except Self.py

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -12,7 +12,7 @@ for i in range(1, len(nums)):        # prefix means left to right multiplication
     res[i] = res[i-1] * nums[i-1]    # res [1 x 1 1] = res[0]*num[0] = 1*1 = x te boshbe  
     print('pre', res)                # res [1 1 x 1] = res[1]*num[1] = 1*2 = x te boshbe
 postfix = 1
-for i in range(len(nums) - 1, -1, -1):   # postfix reverse mul [24,24,12,4] // for each mul Prefix*Postfix
+for i in range(len(nums) - 1, -1, -1):   # postfix reverse mul [24,24,12,4] // for each mul Prefix*Postfix  # [3, 2, 1] 0 hole, -1 hole sob [3, 2, 1, 0]
     res[i] = res[i] * postfix                 # to keep last position fix, mul with 1. postfix =1 res[4] = res[4]*1= res[4]
     print('post', res)                   # multiplies the current value at index i in the res with the current value of the postfix variable.
     postfix = postfix * nums[i]                # portfix = 1 * num[last] = 1*4 = 4 
