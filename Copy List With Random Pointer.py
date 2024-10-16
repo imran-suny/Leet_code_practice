be5553331f0f46ddcab6e869fdd43bc4b940d0c6
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-16 12:38:57 -0400

    Update Copy List With Random Pointer.py

--- Copy List With Random Pointer.py
+++ Copy List With Random Pointer.py
@@ -4,18 +4,18 @@ Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
 
 class Solution:
     def copyRandomList(self, head: "Node") -> "Node":
-        oldToCopy = {None: None}   # cur jokhon null hobe, last one # a hashmap map to map the original nodes to their corresponding new nodes.
+        dict = {None: None}   # cur jokhon null hobe, last one # a hashmap map to map the original nodes to their corresponding new nodes.
 
         cur = head
         while cur:
             copy = Node(cur.val)   # creating new nodes for each original node 
-            oldToCopy[cur] = copy  # updating the map with the mapping between original and new nodes.
+            dict[cur] = copy  # updating the map with the mapping between original and new nodes.
             cur = cur.next
         cur = head
         while cur:
-            copy = oldToCopy[cur]  # update the next and random pointers of the new nodes using the information stored in map
-            copy.next = oldToCopy[cur.next]
-            copy.random = oldToCopy[cur.random]
+            copy = dict[cur]  # update the next and random pointers of the new nodes using the information stored in map
+            copy.next = dict[cur.next]
+            copy.random = dict[cur.random]
             cur = cur.next
-        return oldToCopy[head]     # Return the new head of the linked list, which is the node corresponding to the original head in the map.
+        return dict[head]     # Return the new head of the linked list, which is the node corresponding to the original head in the map.
 
