cb2d5aec71df5aa8897d36df91dc0c6e83036075
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-16 17:34:14 -0400

    Update Lowest Common Ancestor Binary Search Tree.py

--- Lowest Common Ancestor Binary Search Tree.py
+++ Lowest Common Ancestor Binary Search Tree.py
@@ -1,8 +1,7 @@
 
 class Solution:
     def lowestCommonAncestor(
-        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
-    ) -> "TreeNode":
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode" ) -> "TreeNode":
         while True:
             if root.val < p.val and root.val < q.val:    # right a move
                 root = root.right
