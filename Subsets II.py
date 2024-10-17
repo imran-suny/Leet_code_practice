7550b020b3115304238dac9f11508fcf04c4c904
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-17 16:35:54 -0400

    Update Subsets II.py

--- Subsets II.py
+++ Subsets II.py
@@ -4,22 +4,22 @@ The solution set must not contain duplicate subsets. Return the solution in any 
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-
-        def backtrack(i, subset):
+        res = []
+        subset =[]
+        def backtrack(i):
             if i == len(nums):
-                res.append(subset[::])
+                res.append(subset.copy())
                 return
 
             # All subsets that include nums[i]
             subset.append(nums[i])
-            backtrack(i + 1, subset)
+            backtrack(i + 1)
             subset.pop()
             # All subsets that don't include nums[i]
-            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            while i+1  < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
-            backtrack(i + 1, subset)
+            backtrack(i + 1)
 
-        backtrack(0, [])
+        backtrack(0)
         return res
