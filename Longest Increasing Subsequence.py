69e54446653301b87df0c462893574a4f9666aa8
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-19 15:13:11 -0400

    Update Longest Increasing Subsequence.py

--- Longest Increasing Subsequence.py
+++ Longest Increasing Subsequence.py
@@ -7,8 +7,8 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         LIS = [1] * len(nums)                          # [1,1,1,1,1,1,1,1]
 
-        for i in range(len(nums) - 1, -1, -1):         # 7,6,5
-            for j in range(i + 1, len(nums)):          # 101, 18
+        for i in range(len(nums) - 1, -1, -1):         # i = 7, j =8 out of bound...
+            for j in range(i + 1, len(nums)):          # i=6, j =7 num [6] = 101, num[7]= 18 false if condition
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])   # list[5] = 2 
         return max(LIS) 
