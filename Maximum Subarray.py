93256310a6451b6e928b330b6e0cfdd81e899032
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-20 09:21:40 -0400

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -2,7 +2,18 @@ https://leetcode.com/problems/maximum-subarray/description/
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
 
+        total = 0
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res
+        
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         ans = float('-inf')  
