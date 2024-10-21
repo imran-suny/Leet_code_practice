c121c43904284702f32ad4b7efb537d55853d5bb
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-20 22:39:56 -0400

    Update Sum of Two Integers.py

--- Sum of Two Integers.py
+++ Sum of Two Integers.py
@@ -1,19 +1,6 @@
 https://leetcode.com/problems/sum-of-two-integers/solutions/4927043/blind-75-beats-100-00-11-75/
 Input: a = 2, b = 3
 Output: 5
-
-class Solution {             # In Java integers, are by default 32-bit signed two's complement integers
-    public int getSum(int a, int b) {
-        while (b != 0){
-            int carry = (a & b) << 1;
-            a = a ^ b;
-            b = carry;
-        } 
-        return a;
-    }
-}
-
-python: bitShort is initialized to 0xffffffff, which is a 32-bit integer with all bits set to 1 except for the sign bit.
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         while b != 0:
