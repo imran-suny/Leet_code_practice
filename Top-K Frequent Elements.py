5935e23c0ee17bb0a1bb695f61b94f28b6b34542
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-10-24 17:07:39 -0400

    Update Top-K Frequent Elements.py

--- Top-K Frequent Elements.py
+++ Top-K Frequent Elements.py
@@ -4,21 +4,18 @@ Output: [1,2]
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:   
         count = {}
         for n in nums:
-            count[n] = 1 + count.get(n, 0) # {1:3, 2:2, 3:1} // count[1] = 1+ dictionary.get(n, 0)
+            count[n] = 1 + count.get(n, 0) # {1:3, 2:2, 3:1} 
         
-        freq = [[] for i in range(len(nums) + 1)] # [ [], [], [], [], [], [], [] ] # 1ta beshi 0 position a keo boshbe na          
+        freq = [[] for i in range(len(nums) + 1)] # [ [], [], [], [], [], [], [] ] #         
         for n, c in count.items(): # n = index(1), c = count(3) 
-            freq[c].append(n) # [[], [], [], [1], [], [], []] # for each number n and its count c, 
-                                                              #it appends n to the list[] at index c in the freq list.
-            print (freq) # [[], [3], [2], [1], [], [], []]
-
+            freq[c].append(n) # [[], [], [], [1], [], [], []]  #appends index-- 1:3,1 3 bar, so 3 er ghore 1..                                             
+            print (freq) # [[], [3], [2], [1], [], [], []]   # boro ta last a  
         res = []
         for i in range(len(freq) - 1, 0, -1): # #range(6, 0, -1)...6theke start to 0,,,step= -1
             for n in freq[i]: # [[], [3], [2], [1], [], [], []], 6number empty, n =2...
-                res.append(n) # nothing to add// komte komte 3 a jbe, then for n in freq[i=3]/// maybe [3,4,5] thqkte pare freq[i] a
+                res.append(n) # nothing to add// komte komte 3 a jbe, maybe [3,4,5] thqkte pare freq[i]a
                 if len(res) == k: #  res = [1, 2] 
                     return res
