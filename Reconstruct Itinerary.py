bf62e8b55a23f13fef9e84273673b0d95c65f484
Md Imran Hossain <61520633+imran-suny@users.noreply.github.com>
2024-11-18 15:37:52 -0500

    Update Reconstruct Itinerary.py

--- Reconstruct Itinerary.py
+++ Reconstruct Itinerary.py
@@ -15,6 +15,22 @@ class Solution:
         dfs("JFK")
         return itinerary[::-1] 
 
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets)[::-1]:
+            adj[src].append(dst)
+
+        res = []
+        def dfs(src):
+            while adj[src]:
+                dst = adj[src].pop()
+                dfs(dst)
+            res.append(src)
+            
+        dfs('JFK')
+        return res[::-1]
+        
 # tickets =  [["MUC", "LHR"], ["JFK", "MUC"], ["JFK", "ACL"], ["SFO", "SJC"], ["LHR", "SFO"]]
 deque(['ACL', 'MUC'])
 gggggg ['ACL']
